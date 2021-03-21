260636b5706c4d1dcedd2bfb381d38341b1a50f6
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-03-20 22:07:06 -0600

    Fixing lint config

--- projects/nativescript-hide-action-bar/src/lib/hide-action-bar.directive.ts
+++ projects/nativescript-hide-action-bar/src/lib/hide-action-bar.directive.ts
@@ -8,6 +8,7 @@ export class HideActionBarDirective {
 
   /**
    * Hides the `ActionBar` for the page when the directive is loaded.
+   *
    * @param page The NativeScript `Page` reference for the current displayed component.
    */
   constructor(private page: Page) {
--- projects/nativescript-hide-action-bar/src/lib/hide-action-bar.module.ts
+++ projects/nativescript-hide-action-bar/src/lib/hide-action-bar.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HideActionBarDirective } from './hide-action-bar.directive';
 
-
-
 @NgModule({
   declarations: [
     HideActionBarDirective
