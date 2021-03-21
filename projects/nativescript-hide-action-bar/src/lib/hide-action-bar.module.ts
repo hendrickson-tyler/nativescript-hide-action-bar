23465eb931696ec20959212295c6adff7abf227d
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-03-20 20:44:51 -0600

    Copy of old plugin

---
+++ projects/nativescript-hide-action-bar/src/lib/hide-action-bar.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HideActionBarDirective } from './hide-action-bar.directive';
+
+
+
+@NgModule({
+  declarations: [
+    HideActionBarDirective
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HideActionBarDirective
+  ]
+})
+export class HideActionBarModule { }
---
+++ projects/nativescript-hide-action-bar/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of nativescript-hide-action-bar
+ */
+
+export * from './lib/hide-action-bar.directive';
+export * from './lib/hide-action-bar.module';
